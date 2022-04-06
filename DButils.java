a3dc369b2784814f72f3e2aa494f6c381f691dfb
veeralavaws <veeralavaws@gmail.com>
2022-04-06 16:25:09 +0530

    stashing

--- DButils.java
+++ DButils.java
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
+testing stash command
 database commands
-=======
 one
 this is in master branch
 just making some modifications
->>>>>>> 0d69ef2045bc4f57ad3e2c79c19c97ac19c78dd5
 this file contains database related concepts
